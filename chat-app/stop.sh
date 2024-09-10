675718e5b623edda903a6d48d67fcddf833259b6
FelipeMeiga <felipe.maia.meiga@gmail.com>
2024-09-10 09:56:43 -0300

    trabalho 1 concluido

--- chat-app/stop.sh
+++ chat-app/stop.sh
@@ -5,11 +5,13 @@ sudo docker ps -a
 sudo docker stop chat-app_consumer-service_1
 sudo docker stop chat-app_producer-service_1
 sudo docker stop chat-app_kafka_1
-sudo docker stop chat-app_zookeeper_1
+sudo docker stop chat-app_zookeeper1_1
+sudo docker stop chat-app_zookeeper2_1
 
 sudo docker rm chat-app_consumer-service_1
 sudo docker rm chat-app_producer-service_1
 sudo docker rm chat-app_kafka_1
-sudo docker rm chat-app_zookeeper_1
+sudo docker rm chat-app_zookeeper1_1
+sudo docker rm chat-app_zookeeper2_1
 
 sudo docker ps -a
